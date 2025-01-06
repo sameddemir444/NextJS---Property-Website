12650ce33848be9456959164fd57e624763511ce
Samed Demir <demirsamed945@gmail.com>
2025-01-06 21:24:53 +0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 export default function Homepage() {
   return (
     <div>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </div>
   );
 }
