3af549df2fc85f75375c371fcd913d23c150a8a3
Samed Demir <demirsamed945@gmail.com>
2025-01-06 21:54:34 +0300

    changes

--- components/HomeProperties.tsx
+++ components/HomeProperties.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import properties from "@/properties.json";
 import PropertyCard from "./PropertyCard";
 
@@ -5,22 +6,33 @@ const HomeProperties = () => {
   const recentProperties = properties.slice(0, 4);
 
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        <h2 className="text-3xl font-bold text-blue-600 mb-6 text-start">
-          Recent Properties
-        </h2>
-        {properties.length === 0 ? (
-          <p>No properties found</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-            {recentProperties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+    <>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <h2 className="text-3xl font-bold text-blue-600 mb-6 text-start">
+            Recent Properties
+          </h2>
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+              {recentProperties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+
+      <section className="m-auto max-w-lg my-12 px-6">
+        <Link
+          href="/properties"
+          className="block bg-black text-white py-4 px-6 rounded-xl hover:bg-gray-900 text-center font-medium duration-200"
+        >
+          View All Properties
+        </Link>
+      </section>
+    </>
   );
 };
 
