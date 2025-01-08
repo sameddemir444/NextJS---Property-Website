d9b16b99028c4912565e9ca0fd85e90ca0619b2d
Samed Demir <demirsamed945@gmail.com>
2025-01-08 23:21:01 +0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
 
 export default function Homepage() {
+  console.log(process.env.MONGODB_URI);
   return (
     <div>
       <Hero />
